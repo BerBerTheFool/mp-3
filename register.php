a554715f1edc67aa7e9445bf20788ff73480bf30
BerBerTheFool <157215365+BerBerTheFool@users.noreply.github.com>
2024-12-28 19:37:14 +0800

    Update and rename registernow.php to register.php
    the fuck

--- register.php
+++ register.php
@@ -34,7 +34,7 @@ mysqli_close($conn);
     <meta charset="utf-8">  
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
     <title>User Registration Form</title>  
-    <link rel="stylesheet" href="./stylesheet.css">
+    <link rel="stylesheet" href="">
 </head>
 <body>    
     <section>
